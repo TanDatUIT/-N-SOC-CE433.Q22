a234c097c46a742efaaf908b63a8001094e45b1b
dat652728-svg <dat652728@gmail.com>
2026-03-12 22:04:18 +0700

    shift_register.v

--- PROJECT/Median/shift_register.v
+++ PROJECT/Median/shift_register.v
@@ -1,8 +1,18 @@
-module shift_register(
-
-
+module shift_register #(parameter DATA_WIDTH = 8) (
+    input clk,
+    input rst,
+    input [DATA_WIDTH-1:0] a_i,
+    output reg [DATA_WIDTH-1:0] a0, a1, a2, a3
 );
-
-
-
-endmodule 
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            {a0, a1, a2, a3} <= 0;
+        end else begin
+            // a0 <= Gia tri moi 
+            a0 <= a_i;
+            a1 <= a0;
+            a2 <= a1;
+            a3 <= a2;
+        end
+    end
+endmodule
